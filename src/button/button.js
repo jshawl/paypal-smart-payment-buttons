f2859e5cd4ec2c2fca7a6d58d40ee35f213795c7
Nate Bierdeman <nbierdeman@paypal.com>
2022-10-24 15:51:44 -0700

    Add smartWalletOrderID as FPTI_KEY.TOKEN

--- src/button/button.js
+++ src/button/button.js
@@ -261,7 +261,7 @@ export function setupButton({
     const setupButtonLogsTask = setupButtonLogger({
         style, env, sdkVersion, sessionID, clientID, partnerAttributionID, commit, sdkCorrelationID,
         stickinessID, buttonCorrelationID, locale, merchantID, buttonSessionID, merchantDomain,
-        fundingSource, getQueriedEligibleFunding, buyerCountry, onShippingChange, experience, wallet });
+        fundingSource, getQueriedEligibleFunding, buyerCountry, onShippingChange, experience, wallet, smartWalletOrderID });
     const setupPaymentFlowsTask = setupPaymentFlows({ props, config, serviceData, components });
     const setupExportsTask = setupExports({ props, isEnabled, facilitatorAccessToken, fundingEligibility, merchantID });
 
