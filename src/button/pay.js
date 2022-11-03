5d61a620457b2e9926398c366b07b9f88d896cd1
Nate Bierdeman <nbierdeman@paypal.com>
2022-11-03 12:03:51 -0700

    Fix 'token=null' process_checkout_approve bug

--- src/button/pay.js
+++ src/button/pay.js
@@ -99,8 +99,7 @@ export function initiatePaymentFlow({ payment, serviceData, config, components, 
                     [FPTI_KEY.CONTEXT_ID]:         buttonSessionID,
                     [FPTI_KEY.BUTTON_SESSION_UID]: buttonSessionID,
                     [AMPLITUDE_KEY.USER_ID]:       buttonSessionID,
-                    [AMPLITUDE_KEY.TIME]:          Date.now().toString(),
-                    [FPTI_KEY.TOKEN]:              null
+                    [AMPLITUDE_KEY.TIME]:          Date.now().toString()
                 };
             })
             .track({
