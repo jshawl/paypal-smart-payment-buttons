6b54a7cba91042599d5576b8b349435eadf69282
Jesse Shawl <jshawl@paypal.com>
2023-09-07 14:11:51 -0500

    Remove unused file

--- src/props/utils.test.js
+++
@@ -1,9 +0,0 @@
-/* @flow */
-
-import { vi, test, describe } from "vitest";
-
-describe("props utils", () => {
-  describe("onShipping*Change actions", () => {
-    test("tax updates the amount", () => {});
-  });
-});
